9877070e5333bea90e0e02e6d257705fe1201002
nic-d <nicanthonidiaz@gmail.com>
2024-01-02 15:12:48 -0500

    User Model created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = 3001;
+const app = express();
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema for User Model
+const userSchema = new Schema({
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+        type: String, unique: true, required: true, validate: {
+            validator: function (value) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            },
+            message: 'Invalid email address format'
+        },
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// 'friendCount' Virtual that retrieves the length of the user's friends array field on query
+
+userSchema.virtual('friendCount')
+.get(function(){
+    return this.friends.length;
+});
+
+// Initialize User model
+const User = model('user', userSchema);
+
+module.exports = User;
